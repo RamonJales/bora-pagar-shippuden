fc050ad3f4fd09deffe56acbcf5ef1dda4ba4fc1
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-18 21:28:21 -0300

    backend: Initial Commit

---
+++ backend/src/main/java/com/borathings/borapagar/BoraPagarApplication.java
@@ -0,0 +1,13 @@
+package com.borathings.borapagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraPagarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/borathings/borapagar/BorapagarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borathings.borapagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraPagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
