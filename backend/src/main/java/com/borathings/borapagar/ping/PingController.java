ad09a2a356e48f7c69071d39580102c72c2ae5de
Ian Gabriel <ianeigor12@gmail.com>
2024-06-19 18:56:07 -0300

    feat: adding ping route as example

---
+++ backend/src/main/java/com/borathings/borapagar/ping/PingController.java
@@ -0,0 +1,15 @@
+package com.borathings.borapagar.ping;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+	@GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Pong");
+    }
+}
