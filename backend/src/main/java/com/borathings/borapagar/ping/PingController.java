62b7fe35ae5e084f8ac4a34b8f053ed0b4aa287e
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-20 20:45:23 -0300

    doc: Add doc comments to PingController

--- backend/src/main/java/com/borathings/borapagar/ping/PingController.java
+++ backend/src/main/java/com/borathings/borapagar/ping/PingController.java
@@ -5,9 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Classe que servirá de exemplo para como seguir os padrões do projeto
+ */
+
 @RestController
 public class PingController {
 
+	/**
+	 * HTTPHandler que ao receber um GET em "/ping", retornará a string "pong" com status OK (200)
+	 * @see PingController
+	 */
 	@GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("Pong");
