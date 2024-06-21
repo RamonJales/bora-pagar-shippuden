0b8a2e77a6cf2627057aa8ccdb37b5adf915da9e
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-20 22:42:00 -0300

    chore: run formatter

--- backend/src/main/java/com/borathings/borapagar/ping/impl/PingControllerImpl.java
+++ backend/src/main/java/com/borathings/borapagar/ping/impl/PingControllerImpl.java
@@ -1,15 +1,15 @@
 package com.borathings.borapagar.ping.impl;
 
+import com.borathings.borapagar.ping.PingController;
 import java.util.Collections;
 import java.util.List;
-
-import com.borathings.borapagar.ping.PingController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Implementação do @PingController, Controller utilizado para fins de demonstração dos padrões a serem seguidos
+ * Implementação do @PingController, Controller utilizado para fins de demonstração dos padrões a
+ * serem seguidos
  */
 @RestController
 public class PingControllerImpl implements PingController {
@@ -21,7 +21,6 @@ public class PingControllerImpl implements PingController {
     }
 
     /**
-     *
      * @param quantity quantidade de "pongs" que serão retornados
      * @return Uma response com código HTTP 200 e o body com uma lista de `quantity` pongs
      */
@@ -29,5 +28,4 @@ public class PingControllerImpl implements PingController {
         List<String> response = Collections.nCopies(quantity, "pong");
         return ResponseEntity.ok().body(response);
     }
-
 }
