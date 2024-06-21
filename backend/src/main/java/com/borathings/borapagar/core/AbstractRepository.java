79b9936acc499a2b0636e7858ca55dd471bbcdcb
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-20 21:17:04 -0300

    fix: `add @NoRepositoryBean` to AbstractRepository
    Our AbstractRepository needs `@NoRepositoryBean` because otherwise
    spring will try to create a instance of it but it will fail because
    there is not any entity for this repository.
    
    see: https://docs.spring.io/spring-data/jpa/reference/repositories/definition.html#repositories.definition-tuning

--- backend/src/main/java/com/borathings/borapagar/core/AbstractRepository.java
+++ backend/src/main/java/com/borathings/borapagar/core/AbstractRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-@Repository
+@NoRepositoryBean
 public interface AbstractRepository<M extends AbstractModel> extends JpaRepository<M, Long> {
     List<M> findAllActive();
 
