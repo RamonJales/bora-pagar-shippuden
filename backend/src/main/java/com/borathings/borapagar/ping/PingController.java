3821914a9fa6810c6683ae57e7e57d219d0ca7a6
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-20 22:41:16 -0300

    feat: Add Swagger and separate PingController and PingControllerImpl

--- backend/src/main/java/com/borathings/borapagar/ping/PingController.java
+++ backend/src/main/java/com/borathings/borapagar/ping/PingController.java
@@ -1,21 +1,37 @@
 package com.borathings.borapagar.ping;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/** Classe que servirá de exemplo para como seguir os padrões do projeto */
-@RestController
-public class PingController {
+/**
+ * Este é um exemplo de Controller a ser seguido no projeto. Adotamos o padrão descrito em
+ * https://www.baeldung.com/spring-interface-driven-controllers, no qual as annotations necessárias
+ * são colocadas na interface.
+ *
+ * <p>Esse padrão permite que a implementação dos Controllers seja mais "limpa" e focada apenas nos
+ * detalhes técnicos, promovendo uma separação clara entre a definição da API e sua implementação.
+ */
+@RestControllerAdvice
+public interface PingController {
 
     /**
      * HTTPHandler que ao receber um GET em "/ping", retornará a string "pong" com status OK (200)
      *
      * @see PingController
      */
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
+    public ResponseEntity<String> ping();
 
-        return ResponseEntity.ok("Pong");
-    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/pings")
+    public ResponseEntity<List<String>> pings(
+            @Parameter(description = "quantity of pongs to be delivered") @RequestParam
+                    int quantity);
 }
