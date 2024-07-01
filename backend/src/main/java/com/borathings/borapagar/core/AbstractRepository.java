e1a179a8a50e2ac2c8ea069b91dc98ae4464d1e0
Ian Gabriel <ianeigor12@gmail.com>
2024-07-01 20:13:12 -0300

    fix: abstract repository method names'

--- backend/src/main/java/com/borathings/borapagar/core/AbstractRepository.java
+++ backend/src/main/java/com/borathings/borapagar/core/AbstractRepository.java
@@ -1,14 +1,11 @@
 package com.borathings.borapagar.core;
 
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface AbstractRepository<M extends AbstractModel> extends JpaRepository<M, Long> {
-    List<M> findAllActive();
-
-    Page<M> findAllActiveByPage(Pageable pageable);
+    List<M> findAllByDeletedFalse();
+    //Page<M> findAllByDeletedFalseByPage(Pageable pageable);
 }
