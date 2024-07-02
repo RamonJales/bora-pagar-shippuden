cc39ae475bdfd48b2095dc64f459fe5ce37ec449
Ian Gabriel <ianeigor12@gmail.com>
2024-07-01 23:32:44 -0300

    feat: adding javadoc to controller

--- backend/src/main/java/com/borathings/borapagar/course/CourseController.java
+++ backend/src/main/java/com/borathings/borapagar/course/CourseController.java
@@ -6,9 +6,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,19 +18,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequestMapping("/course")
 public interface CourseController {
 
+    /**
+     * Insere um novo curso no banco de dados
+     * @param courseEntityDto - CourseDTO - Dados do curso
+     * @return ResponseEntity<CourseEntity> - Curso criado
+     */
     @PostMapping()
     public ResponseEntity<CourseEntity> createCourse(@Valid @RequestBody CourseDTO courseEntityDto);
 
+    /**
+     * Retorna todos os cursos cadastrados
+     * @return ResponseEntity<List<CourseEntity>> - Lista de cursos
+     */
     @GetMapping()
     public ResponseEntity<List<CourseEntity>> findAllCourses();
 
+    /**
+     * Retorna um curso específico
+     * @param id - Long - ID do curso
+     * @return ResponseEntity<CourseEntity> - Curso específico
+     */
     @GetMapping("/{id}")
     public ResponseEntity<CourseEntity> findCourseById(@RequestParam Long id);
 
-    @PatchMapping("/{id}")
+    /**
+     * Atualiza um curso específico
+     * @param courseEntityDto - CourseDTO - Dados do curso
+     * @param id - Long - ID do curso
+     * @return ResponseEntity<CourseEntity> - Curso atualizado
+     */
+    @PutMapping("/{id}")
     public ResponseEntity<CourseEntity> updateCourse(
             @RequestBody @Valid CourseDTO courseEntityDto, @PathVariable Long id);
 
+    /**
+     * Deleta um curso específico. Se o curso não existir, não faz nada.
+     * Independente do caso, retorna uma mensagem de sucesso.
+     * @param id - Long - ID do curso
+     * @return ResponseEntity<String> - Mensagem de sucesso
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCourse(@PathVariable Long id);
 }
