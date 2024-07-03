dd30fd173c24b38719018d44884615b5bd54c7fa
Emanuel Kywal <emanuelkywal@outlook.com>
2024-07-03 00:12:52 -0300

    style: code indent -> mvn spotless:apply executed

--- backend/src/main/java/com/borathings/borapagar/config/SwaggerConfig.java
+++ backend/src/main/java/com/borathings/borapagar/config/SwaggerConfig.java
@@ -1,12 +1,11 @@
 package com.borathings.borapagar.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,23 +17,16 @@ public class SwaggerConfig {
         Info info = new Info();
         Contact contact = new Contact();
 
-        return openAPI
-            .info(
-                info
-                    .title("Bora Pagar")
-                    .description("Ferramenta de auxílio ao perído de matrícula")
-                    .contact(contact.email("boraPagar@gmail.com"))
-                    .version("v1")
-            ); 
+        return openAPI.info(
+                info.title("Bora Pagar")
+                        .description("Ferramenta de auxílio ao perído de matrícula")
+                        .contact(contact.email("boraPagar@gmail.com"))
+                        .version("v1"));
     }
 
     @Bean
     GroupedOpenApi publicApi() {
 
-        return GroupedOpenApi.builder()
-            .group("controllers")
-            .pathsToMatch("v1/**")
-            .build();
+        return GroupedOpenApi.builder().group("controllers").pathsToMatch("v1/**").build();
     }
-
 }
