f9a080c292685b4c4becb9b065639e972ec78e13
Ian Gabriel <ianeigor12@gmail.com>
2024-07-04 16:05:16 -0300

    fix(course): removing drop migration and altering the first one

--- backend/src/main/resources/db/migration/V1__CREATE_COURSE_TABLE_AND_ENUM.sql
+++ backend/src/main/resources/db/migration/V1__CREATE_COURSE_TABLE_AND_ENUM.sql
@@ -2,6 +2,5 @@ CREATE TABLE course (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     coordinator VARCHAR(255) NOT NULL,
-    course_level VARCHAR(255) NOT NULL,
     deleted BOOLEAN DEFAULT FALSE
 );
