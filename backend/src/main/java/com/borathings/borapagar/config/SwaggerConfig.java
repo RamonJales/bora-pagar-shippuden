52faacf65f4bfb9bdacc94f1a142b53ec8f24a91
Emanuel Kywal <emanuelkywal@outlook.com>
2024-07-13 14:17:27 -0400

    style: execute mvn spotless:apply

--- backend/src/main/java/com/borathings/borapagar/config/SwaggerConfig.java
+++ backend/src/main/java/com/borathings/borapagar/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.borathings.borapagar.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
