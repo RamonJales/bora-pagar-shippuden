236d19f2599dbb09f92c2334c0b10e8f005bdfb3
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-07-17 11:56:17 -0300

    fix: Removed @Data from UserEntity
    Lombok @Data and JPA @Entity does not converge well, a better
    alternative is to write/generate toString(), hashCode() and equals()
    methods

--- backend/src/main/java/com/borathings/borapagar/user/UserEntity.java
+++ backend/src/main/java/com/borathings/borapagar/user/UserEntity.java
@@ -5,23 +5,45 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /** UserEntity */
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 @Builder
 public class UserEntity extends AbstractModel {
     @Column @NotNull private String email;
     @Column @NotNull private String name;
     @Column @NotNull private String googleId;
     @Column private String imageUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+        return getEmail().equals(that.getEmail())
+                && getName().equals(that.getName())
+                && getGoogleId().equals(that.getGoogleId())
+                && Objects.equals(getImageUrl(), that.getImageUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getEmail().hashCode();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getGoogleId().hashCode();
+        result = 31 * result + Objects.hashCode(getImageUrl());
+        return result;
+    }
 }
