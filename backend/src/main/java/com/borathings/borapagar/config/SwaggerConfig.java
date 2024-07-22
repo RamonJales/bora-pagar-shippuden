59e6c78769aa4116d509e36a9742ac9504643248
Emanuel Kywal <emanuelkywal@outlook.com>
2024-07-22 19:59:28 -0300

    Feat: configurar swagger(detalhes) e relátorio/CI com Jacoco   (#30)
    * feat: basic swagger config
    
    * feat: jacoco pom.xml settings
    
    * feat: jacoco exclude and covered percentual limit
    
    * ci: jacoco overall-coverage check
    
    * style: code indent -> mvn spotless:apply executed
    
    * fix: jacoco.xml path
    
    * fix: jacoco.xml path
    
    * fix: jacoco.xml path
    
    * fix: decrease covered ratio minimum limit
    
    * fix: jacoco.xml file path
    
    * fix: remove group OpenAPI and jacoco.xml file path
    
    * style: execute mvn spotless:apply
    
    * fix: decrease overall coverage minimum limit
    
    * fix: decrease overall coverage minimum limit to 40%
    
    * feat: jacoco coverage comment title
    
    * feat: job test-coverage and action generate coverage report
    
    * fix: run container on coverage job
    
    * fix: remove mvn clean of action gen coverage report
    
    * fix: downgrade action version
    
    * fix: specific workflow to test coverage
    
    * fix: syntax error on test_coverage.yml
    
    * fix: maven on test_coverage job
    
    * fix: mvn not found on github action
    
    * refactor: swagger details
    
    * style: mvn spotless:apply
    
    * feat: jacoco PR report prettier
    
    * chore: auto-add @Generated on lombok code
    
    ---------
    
    Co-authored-by: Jose Eduardo Monteiro <mds.josee@gmail.com>

---
+++ backend/src/main/java/com/borathings/borapagar/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.borathings.borapagar.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Bora Pagar")
+                                .description("Ferramenta de auxílio ao perído de matrícula")
+                                .contact(new Contact().email("boraPagar@gmail.com"))
+                                .license(new License().name("License"))
+                                .version("v2"));
+    }
+}
