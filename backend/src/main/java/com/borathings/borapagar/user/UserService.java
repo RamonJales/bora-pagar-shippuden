5c0f70e3bd31099d7c63d7c7430577fe7573df3a
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-07-23 15:20:49 -0300

    docs: add doc comments to UserService

--- backend/src/main/java/com/borathings/borapagar/user/UserService.java
+++ backend/src/main/java/com/borathings/borapagar/user/UserService.java
@@ -13,6 +13,11 @@ public class UserService {
     @Autowired UserRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    /**
+     * upsert vem de update or insert, este método verifica se um OidcUser possui registro no banco
+     * de dados, caso tenha, ele chama o método <code>updateExistingOidcUser</code>, senão, chama o
+     * método <code>insertNewOidcUser</code>
+     */
     public void upsertFromOidcUser(OidcUser oidcUser) {
         String oidcUserGoogleId = oidcUser.getSubject();
         Optional<UserEntity> user = userRepository.findByGoogleId(oidcUserGoogleId);
@@ -21,6 +26,11 @@ public class UserService {
                 () -> insertNewOidcUser(oidcUser));
     }
 
+    /**
+     * Método invocado quando o OidcUser não possui registro equivalente no Banco de Dados, neste
+     * caso é criado um registro novo no banco de dados utilizando as informações do perfil do
+     * google
+     */
     private void insertNewOidcUser(OidcUser oidcUser) {
         String oidcUserEmail = oidcUser.getEmail();
         String oidcUserGoogleId = oidcUser.getSubject();
@@ -38,6 +48,7 @@ public class UserService {
         userRepository.save(userEntity);
     }
 
+    /** Atualiza as informações que podem ter mudado do OidcUser */
     private void updateExistingOidcUser(UserEntity existingUser, OidcUser oidcUser) {
         logger.info(
                 "Usuário com googleId {} de email {} já existe no sistema, atualizando dados",
