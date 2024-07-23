9974948daed4fac68c10f659b54c94a2d9d07694
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-07-23 15:07:42 -0300

    feat: correct implementation of hashCode and equals

--- backend/src/main/java/com/borathings/borapagar/user/UserEntity.java
+++ backend/src/main/java/com/borathings/borapagar/user/UserEntity.java
@@ -11,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 
 /** UserEntity */
 @Entity
@@ -23,27 +24,19 @@ import lombok.Setter;
 public class UserEntity extends AbstractModel {
     @Column @NotNull private String email;
     @Column @NotNull private String name;
-    @Column @NotNull private String googleId;
+    @Column @NotNull @NaturalId private String googleId;
     @Column private String imageUrl;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UserEntity that = (UserEntity) o;
-        return getEmail().equals(that.getEmail())
-                && getName().equals(that.getName())
-                && getGoogleId().equals(that.getGoogleId())
-                && Objects.equals(getImageUrl(), that.getImageUrl());
+        if (!(o instanceof UserEntity)) return false;
+        UserEntity other = (UserEntity) o;
+        return Objects.equals(getGoogleId(), other.getGoogleId());
     }
 
     @Override
     public int hashCode() {
-        int result = getEmail().hashCode();
-        result = 31 * result + getName().hashCode();
-        result = 31 * result + getGoogleId().hashCode();
-        result = 31 * result + Objects.hashCode(getImageUrl());
-        return result;
+        return Objects.hashCode(getGoogleId());
     }
 }
