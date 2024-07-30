d1f00308d9d6ca8ebe853e6735d026adabf02d22
tiago-rodrigues1 <tiagords127@gmail.com>
2024-07-30 12:01:58 -0300

    feat(frontend): create badge component

---
+++ frontend/src/components/BadgeTag.vue
@@ -0,0 +1,7 @@
+<template>
+  <span
+    class="text-center bg-bp_primary-400 bg-opacity-20 text-bp_primary-400 border border-bp_primary-300 font-semibold p-2 rounded-3xl text-xs hover:border-2 cursor-default"
+  >
+    <slot />
+  </span>
+</template>
