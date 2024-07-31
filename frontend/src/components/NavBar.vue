fa4354c55c78b107e4ebded92d39df9a13e53783
Tiago Rodrigues <tiagords127@gmail.com>
2024-07-31 20:28:51 -0300

    Feat: create navbar (#71)
    * chore(frontend): add icons lib
    
    * style(frontend): add colors
    
    * style(frontend): add font
    
    * style(frontend): ++ menu animations and ++ red color
    
    * feat(frontend):  setup icons
    
    * style(frontend): add style class for menu items
    
    * feat(frontend): create navbar component
    
    * feat(frontend): add Navbar component to layout
    
    * refactor(frontend): remove animations
    
    * feat(frontend): create badge component
    
    * refactor(frotend): changes in menu
    
    use BadgeTag component
    changes menu's slide animation logic

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+import BadgeTag from '@/components/BadgeTag.vue'
+
+const isMenuOpen = ref(false)
+
+function handleMenuClick() {
+  isMenuOpen.value = !isMenuOpen.value
+}
+</script>
+
+<template>
+  <header class="w-screen flex items-center gap-10 mb-10 border-b border-bp_neutral-800 p-6">
+    <button
+      type="button"
+      class="text-bp_neutral-400 w-11 h-11 border border-bp_neutral-700 rounded-md"
+      @click="handleMenuClick"
+    >
+      <v-icon name="md-menu-round" scale="1.5" />
+    </button>
+    <div class="flex gap-4 items-center">
+      <img
+        src="https://images.pexels.com/photos/21085439/pexels-photo-21085439/free-photo-of-preto-e-branco-p-b-olhando-vendo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Simbora: o mascote do Bora Pagar"
+        class="h-12 w-12 rounded-full"
+      />
+      <h4 class="text-xl font-bold">Bora Pagar</h4>
+    </div>
+  </header>
+  <nav
+    class="w-11/12 md:w-auto p-5 py-8 bg-neutral-950 absolute top-0 h-screen transition-all ease-in-out duration-300"
+    :class="{
+      '-left-full': !isMenuOpen,
+      'left-0': isMenuOpen
+    }"
+  >
+    <div class="flex items-center justify-between">
+      <div class="flex gap-3 items-center">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-NGEQDekk2BwsllLjk4tcIM_BPIzXECdsg&s"
+          alt="Foto de CR7"
+          class="h-12 w-12 rounded-full"
+        />
+        <span class="font-bold text-lg">CR7 Jales</span>
+      </div>
+      <button type="button" @click="handleMenuClick">
+        <v-icon name="md-close" scale="1.5" />
+      </button>
+    </div>
+    <div class="flex items-center gap-6 text-sm mt-7">
+      <BadgeTag> Tecnologia da Informação </BadgeTag>
+      <BadgeTag>5º Período</BadgeTag>
+    </div>
+
+    <hr class="my-6" />
+
+    <ul class="flex flex-col gap-4">
+      <RouterLink class="menu-item" to="/">
+        <v-icon name="md-home" />
+        <span>Home</span>
+      </RouterLink>
+      <RouterLink class="menu-item" to="/">
+        <v-icon name="md-menubook" />
+        <span>Disciplinas</span>
+      </RouterLink>
+      <RouterLink class="menu-item" to="/">
+        <v-icon name="md-group" />
+        <span>Amigos</span>
+      </RouterLink>
+      <RouterLink class="menu-item" to="/">
+        <v-icon name="md-person" />
+        <span>Perfil</span>
+      </RouterLink>
+      <RouterLink
+        class="flex items-center gap-4 mt-4 text-bp_danger hover:border-b border-bp_danger"
+        to="/"
+      >
+        <v-icon name="md-logout" />
+        <span>Sair</span>
+      </RouterLink>
+    </ul>
+  </nav>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import * as MdIcons from 'oh-vue-icons/icons/md'
+
+const Md = Object.values({ ...MdIcons })
+addIcons(...Md)
+
 const app = createApp(App)
 
 app.use(router)
-
+app.component('v-icon', OhVueIcon)
 app.mount('#app')
