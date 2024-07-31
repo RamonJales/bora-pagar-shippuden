72ade50b103428b994b30aa0c9f30dc09e35288a
Tiago Rodrigues <tiagords127@gmail.com>
2024-07-31 20:28:51 -0300

    Feat: create navbar (#71)
    * chore(frontend): add icons lib
    
    * style(frontend): add colors
    
    * style(frontend): add font
    
    * style(frontend): ++ menu animations and ++ red color
    
    * feat(frontend):  setup icons
    
    * style(frontend): add style class for menu items
    
    * feat(frontend): create navbar component
    
    * feat(frontend): add Navbar component to layout
    
    * refactor(frontend): remove animations
    
    * feat(frontend): create badge component
    
    * refactor(frotend): changes in menu
    
    use BadgeTag component
    changes menu's slide animation logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl text-blue-500">Bora Pagar</h1>
+  <div class="h-screen w-screen bg-bp_neutral-900 text-bp_neutral-50">
+    <NavBar />
+    <h1 class="text-3xl text-blue-500">Bora Pagar</h1>
+  </div>
 </template>
