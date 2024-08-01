9ce60f4af34fe7d89b2f15330fed568cf8017de2
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-01 10:44:44 -0300

    feat(frontend): add Dashboard view (wip)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
-  <main>
-    Home
+  <main class="container mx-auto p-6 xl:max-w-7xl">
+    <header class="flex items-center justify-between border-b border-bp_neutral-800 pb-4">
+      <h1 class="text-3xl font-bold">período-atual · 5º</h1>
+      <span class="text-3xl font-bold text-bp_primary-100">210h</span>
+    </header>
+    <section class="mt-6">
+      <div class="bg-bp_neutral-800 rounded-md h-40 w-64 flex flex-col items-center justify-center">
+        <p class="text-center font-bold w-11/12">
+          Práticas de Leitura e Escrita em Português I
+        </p>
+        <div class="w-full border-t-2 border-bp_neutral-700 d-flex items-center justify-between">
+          <button type="button" class="py-2 px-4 bg-">Concluída</button>
+          <button type="button" class="py-2 px-4 bg-bp_danger-light bg-opacity-15 rounded-md text-bp_danger-light">Remover</button>
+        </div>
+      </div>
+    </section>
+    <section></section>
   </main>
 </template>
