146540ae182efa9e4274d68ced947ad3bc55d2c8
Natalya Nunes <nunes.natalya23@gmail.com>
2024-08-01 08:01:56 -0300

    feat: update components

--- frontend/src/components/ConsultSubjects.vue
+++ frontend/src/components/ConsultSubjects.vue
@@ -4,16 +4,29 @@ import InputSearch from './InputSearch.vue';
 </script>
 
 <template>
-    <header class="bg-bp_neutral-900">
+    <header class="bg-bp_neutral-900 space-y-6">
         <p class="text-bp_neutral-50 text-2xl font-bold">consultar-disciplinas</p>
         <hr>
-        <div>
+        <div class="flex space-x-6">
             <InputSearch/>
-            <div>
-                <button class=" bg-bp_neutral-800 text-bp_neutral-50  border border-bp_neutral-600 rounded-md p-2">Departamento </button>
-                <v-icon name="ri-arrow-drop-down-fill" scale="1.5" />
+            <div class="flex space-x-3">
+                <div>
+                    <button class="card-options">Departamento </button>
+                    <v-icon class="text-bp_primary-" name="ri-arrow-drop-down-fill" scale="1.5" />
+                </div>
+                <div>
+                    <button class="card-options">Dificuldade </button>
+                    <v-icon class="text-bp_primary-" name="ri-arrow-drop-down-fill" scale="1.5" />
+                </div>
+                <div>
+                    <button class="card-options">Horas </button>
+                    <v-icon class="text-bp_primary-300" name="ri-arrow-drop-down-fill" scale="1.5" />
+                </div>
             </div>
         </div>
-        <CardSubjects/>
+        <div class="space-y-4">
+            <CardSubjects/>
+            <CardSubjects/>
+        </div>       
     </header>
 </template>
