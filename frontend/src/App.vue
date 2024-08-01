c790663f65126ab3e1bfba604632e144f9e9efca
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-01 10:44:09 -0300

    feat(frontend): add RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,6 @@ import NavBar from '@/components/NavBar.vue'
 <template>
   <div class="h-screen w-screen bg-bp_neutral-900 text-bp_neutral-50">
     <NavBar />
-    <h1 class="text-3xl text-blue-500">Bora Pagar</h1>
+    <RouterView />
   </div>
 </template>
