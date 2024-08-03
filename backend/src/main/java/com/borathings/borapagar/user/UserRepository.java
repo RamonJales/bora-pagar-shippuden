c4d3fe6c73912a24f6b185fff3e7c9a43dd76381
José Eduardo Monteiro <51955049+joseemds@users.noreply.github.com>
2024-08-02 21:29:43 -0300

    Revert " feat: Adiciona criação de usuário no login (#49)"
    This reverts commit b42b57c39e4c97b4ebfe6b070021281e4b62e2e8.

--- backend/src/main/java/com/borathings/borapagar/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.borathings.borapagar.user;
-
-import com.borathings.borapagar.core.AbstractRepository;
-import java.util.Optional;
-
-/** UserRepository */
-public interface UserRepository extends AbstractRepository<UserEntity> {
-    Optional<UserEntity> findByGoogleId(String googleId);
-}
