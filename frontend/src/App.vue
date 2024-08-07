c44fdafc9ff658e80b46851e3c19098ab6a53e1e
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-07 19:04:46 -0300

    style(frontend): change height prop

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen bg-bp_neutral-900 text-bp_neutral-50">
+  <div class="min-h-screen h-full w-screen bg-bp_neutral-900 text-bp_neutral-50">
     <NavBar />
     <RouterView />
   </div>
