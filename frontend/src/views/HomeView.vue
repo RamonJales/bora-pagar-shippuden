ce6b3499e5fc5ec44123cae216a27d0d9bb493cb
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-07 19:06:52 -0300

    feat(frontend): use new components in home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,67 @@
+<script setup lang="ts">
+import PeriodCard from '@/components/PeriodCard.vue'
+import RadialProgress from '@/components/RadialProgress.vue';
+import SubjectCard from '@/components/SubjectCard.vue'
+</script>
+
 <template>
-  <main class="container mx-auto p-6 xl:max-w-7xl">
+  <main class="h-full container mx-auto p-6 xl:max-w-7xl">
     <header class="flex items-center justify-between border-b border-bp_neutral-800 pb-4">
-      <h1 class="text-3xl font-bold">período-atual · 5º</h1>
-      <span class="text-3xl font-bold text-bp_primary-100">210h</span>
+      <h1 class="title-h1">período-atual · 5º</h1>
+      <span class="title-h1 text-bp_primary-100">210h</span>
     </header>
-    <section class="mt-6">
-      <div class="bg-bp_neutral-800 rounded-md h-40 w-64 flex flex-col items-center justify-center">
-        <p class="text-center font-bold w-11/12">
-          Práticas de Leitura e Escrita em Português I
-        </p>
-        <div class="w-full border-t-2 border-bp_neutral-700 d-flex items-center justify-between">
-          <button type="button" class="py-2 px-4 bg-">Concluída</button>
-          <button type="button" class="py-2 px-4 bg-bp_danger-light bg-opacity-15 rounded-md text-bp_danger-light">Remover</button>
+    
+    <section class="max-w-full my-6 grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <SubjectCard />
+      <SubjectCard />
+      <SubjectCard />
+      <SubjectCard />
+      <SubjectCard />
+      <SubjectCard />
+      <SubjectCard />
+    </section>
+
+    <section class="mt-12 w-full">
+      <header class="w-full md:w-3/5 flex items-center justify-between flex-wrap gap-4 :sm:gap-0">
+        <h1 class="title-h1">período</h1>
+        <button
+          type="button"
+          class="flex items-center justify-center gap-2 bg-bp_primary-400 h-10 px-4 rounded-lg font-semibold"
+        >
+          <v-icon name="md-add" scale="1.25" />
+          Adicionar Semestre
+        </button>
+      </header>
+
+      <div class="w-full flex flex-col md:flex-row items-start justify-between gap-6 mt-8">
+        <div class="w-full md:w-3/5 flex flex-col gap-6">
+          <PeriodCard />
         </div>
+
+        <aside class="w-full md:w-2/5 lg:w-1/3">
+          <div class="bg-bp_neutral-800 p-6 rounded-lg flex flex-col items-center gap-3">
+            <div class="w-full flex items-center justify-between mb-6">
+              <h4 class="font-bold text-xl">Evolução</h4>
+              <span class="text-bp_primary-50 cursor-pointer">Mais</span>
+            </div>
+
+            <RadialProgress progress="85"/>
+
+            <p class="mt-6 w-full flex items-center justify-between">
+              <span>C.H. obrigatória pendente</span>
+              <span class="text-bp_primary-100">720</span>
+            </p>
+            <p class="w-full flex items-center justify-between">
+              <span>C.H. optativa pendente</span>
+              <span class="text-bp_primary-100">720</span>
+            </p>
+            <p class="w-full flex items-center justify-between">
+              <span>C.H. total do curso</span>
+              <span class="text-bp_primary-100">2600</span>
+            </p>
+          </div>
+        </aside>
       </div>
     </section>
-    <section></section>
   </main>
 </template>
