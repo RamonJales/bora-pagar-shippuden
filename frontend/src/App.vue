cad1daf9052f698e8db112013b709a335f504e5c
Danillo Fonseca <127620804+DanilloFonseca@users.noreply.github.com>
2024-08-11 13:17:53 -0300

    Feat/friends screen (#77)
    * feat: friends screen
    
    * feat: BadgeTag with new props: size and type
    
    * feat: reactive ListItemFriend
    
    * fix: fixed all reviews appointments
    
    * fix: changes to fix the style by according the figma
    
    * fix: improve badgeTag component to apply hover
    
    ---------
    
    Co-authored-by: Alisson Diogo <alisson.diogo.soares@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-import NavBar from '@/components/NavBar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="h-screen w-screen bg-bp_neutral-900 text-bp_neutral-50">
-    <NavBar />
-    <h1 class="text-3xl text-blue-500">Bora Pagar</h1>
-  </div>
+  <nav></nav>
+
+  <RouterView />
 </template>
