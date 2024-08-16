42bb6f8034059057514db5f76f26a4106d27e3fc
Natálya Gabriele <nunes.natalya23@gmail.com>
2024-08-16 20:59:13 -0300

    Feat/consult subjects (#79)
    * feat: create consult subjects
    
    * feat: create input base
    
    * feat: update Card Subjects
    
    * feat: update Input Serach
    
    * feat: update components
    
    * feat(frontend): code formatting
    
    * feat(frontend): create button
    
    * feat(frontend): update cardSubjects
    
    * feat(frontend): update interface ConsultaSubjescts
    
    * fix: correções no padrão de projeto
    
    * feat(frontend): add new color
    
    * feat(frontend): add reatividade
    
    * feat(frontend): formatação
    
    * feat(frontend): ajustes css
    
    * feat(frontend): update select
    
    * refactor(frontend): rename props
    
    ---------
    
    Co-authored-by: Alisson Diogo <alisson.diogo.soares@gmail.com>

---
+++ frontend/src/components/InputBase.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex">
+    <input
+      type="text"
+      class="w-full bg-bp_neutral-800 border border-bp_neutral-700 rounded-l-lg py-2 px-4 text-bp_neutral-5"
+      placeholder="pré requisito de fmciii |"
+    />
+  </div>
+</template>
