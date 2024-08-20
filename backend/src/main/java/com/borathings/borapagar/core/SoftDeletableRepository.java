5a99a1dcf94f234ab06dc886e2f6b87819826dfb
Ian Gabriel <ianeigor12@gmail.com>
2024-08-20 11:59:52 -0300

    fix: making softDelete query valid

--- backend/src/main/java/com/borathings/borapagar/core/SoftDeletableRepository.java
+++ backend/src/main/java/com/borathings/borapagar/core/SoftDeletableRepository.java
@@ -1,5 +1,7 @@
 package com.borathings.borapagar.core;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 
@@ -8,6 +10,8 @@ import org.springframework.data.repository.NoRepositoryBean;
 public interface SoftDeletableRepository<M extends SoftDeletableModel>
         extends AbstractRepository<M> {
 
+    @Modifying
+    @Transactional
     @Query("UPDATE #{#entityName} e SET e.deletedAt = CURRENT_TIMESTAMP WHERE e.id = ?1")
-    void softDelete(Long id);
+    public void softDelete(Long id);
 }
