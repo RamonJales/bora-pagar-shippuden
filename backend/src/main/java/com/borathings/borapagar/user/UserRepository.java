ae4d48fedb2d3ce4933691e1fee73a3f7f3085a6
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-08-20 23:52:43 -0300

    fix: add @Repository to UserRepository

--- backend/src/main/java/com/borathings/borapagar/user/UserRepository.java
+++ backend/src/main/java/com/borathings/borapagar/user/UserRepository.java
@@ -2,8 +2,10 @@ package com.borathings.borapagar.user;
 
 import com.borathings.borapagar.core.AbstractRepository;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
 /** UserRepository */
+@Repository
 public interface UserRepository extends AbstractRepository<UserEntity> {
     Optional<UserEntity> findByGoogleId(String googleId);
 }
