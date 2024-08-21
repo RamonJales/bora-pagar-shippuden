9a9e588f6d1a6ef8a4794fd18cf5367761daf006
Natalya Nunes <nunes.natalya23@gmail.com>
2024-08-21 16:34:25 -0300

    feat(frontend): update css details and placeholder

--- frontend/src/components/InputBase.vue
+++ frontend/src/components/InputBase.vue
@@ -1,9 +1,18 @@
+<script setup>
+const props = defineProps({
+  placeholder: {
+    type: String,
+    defalut: 'pré requisito de fcmiii |'
+  }
+})
+</script>
+
 <template>
   <div class="flex">
     <input
       type="text"
-      class="w-full bg-bp_neutral-800 border border-bp_neutral-700 rounded-l-lg py-2 px-4 text-bp_neutral-5"
-      placeholder="pré requisito de fmciii |"
+      class="w-full bg-bp_neutral-900 border border-bp_neutral-700 rounded py-2 px-4 text-bp_neutral-500"
+      :placeholder="props.placeholder"
     />
   </div>
 </template>
