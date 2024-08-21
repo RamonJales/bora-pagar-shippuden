181dc5fff4c675e6310d596a2d497461380cfe18
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-08-20 23:53:15 -0300

    feat: support Jwt and OidcUser in authentication param

--- backend/src/main/java/com/borathings/borapagar/user/impl/UserControllerImpl.java
+++ backend/src/main/java/com/borathings/borapagar/user/impl/UserControllerImpl.java
@@ -7,7 +7,7 @@ import com.borathings.borapagar.user.UserService;
 import com.borathings.borapagar.user.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
 /** UserControllerImpl */
@@ -17,7 +17,7 @@ public class UserControllerImpl implements UserController {
     @Autowired UserService userService;
 
     @Override
-    public ResponseEntity<UserDTO> getCurrentUser(OidcUser currentUser) {
+    public ResponseEntity<UserDTO> getCurrentUser(Authentication currentUser) {
         String currentUserGoogleId = currentUser.getName();
         UserEntity loggedUser = userService.findByGoogleIdOrError(currentUserGoogleId);
         return ResponseEntity.ok(UserMapper.toDTO(loggedUser));
