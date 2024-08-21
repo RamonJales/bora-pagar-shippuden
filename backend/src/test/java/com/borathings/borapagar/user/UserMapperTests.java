62b4ff7b30dd43a392f3f0da2047c5ea6fe7f273
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-08-21 00:06:22 -0300

    test: add UserMapper test

---
+++ backend/src/test/java/com/borathings/borapagar/user/UserMapperTests.java
@@ -0,0 +1,31 @@
+package com.borathings.borapagar.user;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.borathings.borapagar.user.dto.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMapperTest {
+
+    @Test
+    void testToDTO() {
+        UserEntity userEntity =
+                UserEntity.builder()
+                        .id(1L)
+                        .email("ramon.jales.cr7@ufrn.edu.br")
+                        .name("Ramon Jales")
+                        .imageUrl("cr7.jpeg")
+                        .build();
+
+        // Act
+        UserDTO userDTO = UserMapper.toDTO(userEntity);
+
+        // Assert
+        assertEquals(userEntity.getId(), userDTO.id());
+        assertEquals(userEntity.getEmail(), userDTO.email());
+        assertEquals(userEntity.getName(), userDTO.name());
+        assertEquals(userEntity.getImageUrl(), userDTO.imageUrl());
+    }
+}
