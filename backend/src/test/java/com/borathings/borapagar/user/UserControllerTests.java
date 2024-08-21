b0d12b22904756f9d3eb21bd73248e4c3fcf71c1
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-08-20 23:00:42 -0300

    feat(UserControllerTests): shouldGetAuthenticatedUser

--- backend/src/test/java/com/borathings/borapagar/user/UserControllerTests.java
+++ backend/src/test/java/com/borathings/borapagar/user/UserControllerTests.java
@@ -2,12 +2,21 @@ package com.borathings.borapagar.user;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 
 import com.borathings.borapagar.utils.AuthenticatedMockMvc;
@@ -17,6 +26,7 @@ import com.borathings.borapagar.utils.AuthenticatedMockMvc;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureWebMvc
 public class UserControllerTests {
+    @Autowired private MockMvc mockMvc;
     private UserEntity user;
 
     private OidcUser oidcUser;
@@ -40,4 +50,15 @@ public class UserControllerTests {
         when(oidcUser.getSubject()).thenReturn(user.getGoogleId());
     }
     
+    @Test
+    public void shouldGetAuthenticatedUser() throws Exception {
+        this.mockMvc
+                .perform(get("/api/users/me").with(jwt().jwt(jwt -> jwt.subject(user.getGoogleId()))))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(user.getId()))
+                .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.name").value(user.getName()))
+                .andExpect(jsonPath("$.imageUrl").value(user.getImageUrl()));
+    }
 }
