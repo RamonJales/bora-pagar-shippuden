9b27fd751425ac440731c6bb7ce1075c8059f079
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2024-08-22 01:38:40 -0300

    feat: dark-mode

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
 <template>
-  <main>Home</main>
+  <main class="bg-black text-white rounded-md dark:bg-white dark:text-black rounded-md h-screen">
+    Home
+    <div class="flex">
+      <span class="text-white dark:text-black"> {{ isDark ? 'Dark' : 'Light' }} Mode </span>
+      <button
+        @click="toggleDark()"
+        class="py-2 px-4 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
+      ></button>
+    </div>
+  </main>
 </template>
