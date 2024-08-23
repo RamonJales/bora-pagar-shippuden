ffb3106868bdd83dcd9503b3ef8ea29972422009
Ian Gabriel <ianeigor12@gmail.com>
2024-08-23 10:19:38 -0300

    feat: prerequisite relationship

--- backend/src/main/java/com/borathings/borapagar/subject/SubjectEntity.java
+++ backend/src/main/java/com/borathings/borapagar/subject/SubjectEntity.java
@@ -3,15 +3,20 @@ package com.borathings.borapagar.subject;
 import com.borathings.borapagar.classroom.ClassroomEntity;
 import com.borathings.borapagar.core.SoftDeletableModel;
 import com.borathings.borapagar.department.DepartmentEntity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.micrometer.common.lang.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
 import java.util.Set;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,7 +25,6 @@ import lombok.experimental.SuperBuilder;
 @Entity(name = "subject")
 @Getter
 @Setter
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
@@ -36,5 +40,34 @@ public class SubjectEntity extends SoftDeletableModel {
     @OneToMany(mappedBy = "subject")
     private Set<ClassroomEntity> classrooms;
 
-    @ManyToOne @NotNull private DepartmentEntity department;
+    @JsonManagedReference @ManyToOne @NotNull private DepartmentEntity department;
+
+    // Set<SubjectEntity> não funciona aqui por um motivo que desconheço (fica sempre vazio)
+    @ManyToMany
+    @JoinTable(
+            name = "subject_prerequisite",
+            joinColumns = @JoinColumn(name = "subject_id"),
+            inverseJoinColumns = @JoinColumn(name = "prerequisite_id"))
+    @JsonManagedReference
+    private Set<SubjectEntity> prerequisites;
+
+    @ManyToMany(mappedBy = "prerequisites")
+    @JsonBackReference
+    private Set<SubjectEntity> prerequisiteOf;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SubjectEntity)) return false;
+        SubjectEntity other = (SubjectEntity) o;
+        return Objects.equals(getId(), other.getId())
+                && Objects.equals(getName(), other.getName())
+                && Objects.equals(getCode(), other.getCode())
+                && Objects.equals(getSyllabus(), other.getSyllabus());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 }
