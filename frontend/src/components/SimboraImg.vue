17a5d63af1c337329b5c64a2da9863381c15ad70
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-24 23:56:40 -0300

    feat(frontend): add prop size

--- frontend/src/components/SimboraImg.vue
+++ frontend/src/components/SimboraImg.vue
@@ -1,7 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['type', 'size'])
+
+const sizes = {
+  small: 'h-10 w-10',
+  medium: 'w-12 h-12',
+  large: 'h-16 w-16'
+}
+
+const sizeClass = computed(() => {
+  return sizes[props.size] || sizes['medium']
+})
+</script>
+
 <template>
   <img
     src="/images/simbora.png"
     alt="Simbora: o mascote do Bora Pagar"
-    class="h-12 w-12 rounded-full bg-bp_primary-50"
+    :class="sizeClass"
+    class="rounded-full bg-bp_primary-50"
   />
 </template>
