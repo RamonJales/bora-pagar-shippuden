a27c89812301f93e136347beeeb4e9c06a59c634
Natalya Nunes <nunes.natalya23@gmail.com>
2024-08-28 14:10:52 -0300

    feat(frontend): update class

--- frontend/src/components/InputBase.vue
+++ frontend/src/components/InputBase.vue
@@ -8,11 +8,9 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex">
-    <input
-      type="text"
-      class="w-full bg-bp_neutral-900 border border-bp_neutral-700 rounded py-2 px-4 text-bp_neutral-500"
-      :placeholder="props.placeholder"
-    />
-  </div>
+  <input
+    type="text"
+    class="w-full py-4 bg-bp_neutral-900 border border-bp_neutral-700 rounded py-2 px-4 text-bp_neutral-500"
+    :placeholder="props.placeholder"
+  />
 </template>
