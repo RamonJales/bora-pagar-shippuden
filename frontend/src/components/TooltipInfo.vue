b564ef2103fae909644caf806f20c32710aa7f00
Natálya Gabriele <nunes.natalya23@gmail.com>
2024-08-29 12:04:39 -0300

    feat(frontend): correction color and font-size

--- frontend/src/components/TooltipInfo.vue
+++ frontend/src/components/TooltipInfo.vue
@@ -13,12 +13,12 @@ export default {
   <div class="relative group flex items-center">
     <v-icon name="md-infooutline" scale="1.5" />
     <div
-      class="absolute bottom-full mb-2 w-64 p-2 bg-bp_neutral-600 text-white text-xs rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-400 break-words z-50 left-1/2 transform -translate-x-1/2"
+      class="absolute bottom-full mb-2 w-72 p-2 bg-bp_neutral-800 text-white text-xxs md:text-xs rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-400 break-words z-50 left-1/2 transform -translate-x-1/2"
     >
       {{ text }}
       <v-icon
         name="md-playarrow-sharp"
-        class="absolute left-1/2 transform -translate-x-1/2 -bottom-2.5 rotate-90 text-bp_neutral-600"
+        class="absolute left-1/2 transform -translate-x-1/2 -bottom-2.5 rotate-90 text-bp_neutral-800"
         scale="1.0"
       />
     </div>
