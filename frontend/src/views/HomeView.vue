8c61a28ab8a3ab36a0d49c5abdb8996519ea2e4f
DanilloFonseca <danillo.fonseca.129@ufrn.edu.br>
2024-09-03 00:08:51 -0300

    feat: darkmode

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,17 +3,28 @@ import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+console.log(isDark.value);
 </script>
 
 <template>
   <main class="bg-black text-white rounded-md dark:bg-white dark:text-black rounded-md h-screen">
     Home
     <div class="flex">
-      <span class="text-white dark:text-black"> {{ isDark ? 'Dark' : 'Light' }} Mode </span>
+      <span class="text-white dark:text-black mr-2"> {{ isDark ? 'Light ' : 'Dark ' }} Mode </span>
       <button
+        v-if="isDark"
         @click="toggleDark()"
-        class="py-2 px-4 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
-      ></button>
+        class="flex items-center justify-between py-1 px-1 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
+      >
+        <v-icon class="mr-2 ml-2" name="md-darkmode-outlined" scale="1.1"></v-icon>
+      </button>
+      <button
+        v-else
+        @click="toggleDark()"
+        class="flex items-center justify-between py-1 px-1 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
+      >
+        <v-icon class="mr-2 ml-2" name="md-lightmode-outlined" scale="1.1"></v-icon>
+      </button>
     </div>
   </main>
 </template>
