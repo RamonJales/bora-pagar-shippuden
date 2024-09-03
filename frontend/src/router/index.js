ba4bf52e607e68459f396d2710e9626c841f27b1
tiago-rodrigues1 <tiagords127@gmail.com>
2024-09-02 23:18:45 -0300

    refactor: change filename

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ConsultSubjectsView from '@/views/ConsultSubjectsView.vue'
 import DashboardView from '@/views/DashboardView.vue'
-import FriendsScreen from '@/views/FriendsScreen.vue'
+import FriendsScreen from '@/views/FriendsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
