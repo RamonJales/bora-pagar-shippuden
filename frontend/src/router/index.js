4e2c6534546bd8b94197f305fb7ad1a41cd703e4
Natalya Nunes <nunes.natalya23@gmail.com>
2024-09-06 16:21:05 -0300

    feat(frontend): create landingpage

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,20 @@ import HomeView from '../views/HomeView.vue'
 import ConsultSubjectsView from '@/views/ConsultSubjectsView.vue'
 import DashboardView from '@/views/DashboardView.vue'
 import FriendsView from '@/views/FriendsView.vue'
+import LandingPageView from '@/views/LandingPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/',
+      name: 'landing-page',
+      component: LandingPageView
     },
     {
       path: '/disciplinas',
