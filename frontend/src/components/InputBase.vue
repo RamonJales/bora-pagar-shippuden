68c138de7414bf48caab24df3f93ebaca882ae60
Natalya Nunes <nunes.natalya23@gmail.com>
2024-09-06 10:38:37 -0300

    feat(frontend): color correction

--- frontend/src/components/InputBase.vue
+++ frontend/src/components/InputBase.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 <template>
   <input
     type="text"
-    class="w-full py-4 bg-bp_neutral-900 border border-bp_neutral-700 rounded py-2 px-4 text-bp_neutral-500"
+    class="w-full py-4 bg-bp_neutral-800 border border-bp_neutral-700 rounded py-2 px-4 text-bp_neutral-50  placeholder:text-bp_neutral-700"
     :placeholder="props.placeholder"
   />
 </template>
