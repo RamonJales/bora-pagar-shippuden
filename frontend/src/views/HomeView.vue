1459f7e9bb8201ec0b3d7beae6f304bd5902217c
DanilloFonseca <danillo.fonseca.129@ufrn.edu.br>
2024-09-08 00:45:19 -0300

    fix: darkmode colors

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,24 +7,19 @@ console.log(isDark.value);
 </script>
 
 <template>
-  <main class="bg-black text-white rounded-md dark:bg-white dark:text-black rounded-md h-screen">
+  <main class="bg-white text-black rounded-md dark:bg-black dark:text-white h-screen">
     Home
     <div class="flex">
-      <span class="text-white dark:text-black mr-2"> {{ isDark ? 'Light ' : 'Dark ' }} Mode </span>
+      <span class="text-black dark:text-white mr-2"> {{ isDark ? 'Dark ' : 'Light ' }} Mode </span>
       <button
-        v-if="isDark"
         @click="toggleDark()"
-        class="flex items-center justify-between py-1 px-1 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
+        class="flex items-center justify-between py-1 px-1 bg-white text-black border border-black dark:bg-black dark:text-white dark:border-white rounded-md"
       >
-        <v-icon class="mr-2 ml-2" name="md-darkmode-outlined" scale="1.1"></v-icon>
+        <v-icon
+          :name="isDark ? 'md-darkmode-outlined' : 'md-lightmode-outlined'"
+          class="mr-2 ml-2" scale="1.1"></v-icon>
       </button>
-      <button
-        v-else
-        @click="toggleDark()"
-        class="flex items-center justify-between py-1 px-1 bg-black text-white border border-white dark:bg-white dark:text-black dark:border-black rounded-md"
-      >
-        <v-icon class="mr-2 ml-2" name="md-lightmode-outlined" scale="1.1"></v-icon>
-      </button>
+      
     </div>
   </main>
 </template>
