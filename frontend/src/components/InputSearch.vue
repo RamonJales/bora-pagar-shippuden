dd55f1e8a50832a69b01d9302f2037d68928ab6a
tiago-rodrigues1 <tiagords127@gmail.com>
2024-09-12 11:07:38 -0300

    feat(frontend): InputSearch recieves placeholder and id as props

--- frontend/src/components/InputSearch.vue
+++ frontend/src/components/InputSearch.vue
@@ -1,12 +1,13 @@
 <script setup>
-import InputBase from './InputBase.vue'
+const props = defineProps(['inputPlaceholder', 'inputId'])
 </script>
 <template>
   <div class="flex">
     <input
       type="text"
-      class="w-full bg-bp_neutral-800 border border-bp_neutral-700 rounded-l-lg py-2 px-4 text-bp_neutral-500"
-      placeholder="pré requisito de fmciii |"
+      class="w-full bg-bp_neutral-800 border border-bp_neutral-700 rounded-l-lg py-2 px-4 placeholder:text-bp_neutral-500"
+      :placeholder="props.inputPlaceholder"
+      :id="props.inputId"
     />
     <button class="bg-bp_primary-300 text-bp_primary-50 rounded-r-lg py-2 px-4">
       <v-icon name="md-search" scale="1.5" />
